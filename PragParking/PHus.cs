35ed74d1b7073a8ce1eeeafef029d62612e6864a
The Stig <mstig@student.chalmers.se>
2021-03-22 10:37:54 +0100

    skapat PHus

---
+++ PragParking/PHus.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PragParking
+{
+    class PHus
+    {
+        public List <PRuta> PRutor
+        {
+            get
+            {
+
+            }
+            set
+            {
+
+            }
+        }
+    }
+}
