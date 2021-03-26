84d024889f1b7bb57eba90262334d4ff5cc2d264
Per Edward Nilsen <nilliz85@gmail.com>
2021-03-26 13:45:24 +0100

    städade först koden..
    sedan kommenterade jag bort koden som var dublett (första koden vi skrev då den var stökig och primitiv)
    
    sedan så byggde jag vidare på den metoden som parkerade en bil då när jag gjorde den var man tvungen att lägga till ett steg för varje fordon.
    något man nu inte behöver.
    
    jag öppnade sedan mitt "gamla projekt" dör jag hade börjat på uppgiften... kollade igenom den några gånger
    sedan tog jag min meny från den och gjorde om lite så den skulle funka här.
    en meny med ett switch statement där man nu har 3 val.
    
    1) parkera fordon
    2) kolla efter lediga platser
    3) exit
    
    jag raderade sedan dublett koden
    och gjorde så att metoden "hittaLedigPRuta()" "Snurrar" i menyn,
    så att när du parkerat ett fordon skickar switchen tillbaka dig till huvudmenyn där du kan välja mellan att parkera flera fordon eller kolla efter lediga platser.
    
    nu parkeras 1 fordon i taget istället för 2 som det var innan och man kan mellan varje fordon kolla hur många lediga platser som finns kvar

--- PragParking/PHus.cs
+++ PragParking/PHus.cs
@@ -33,29 +33,30 @@ namespace PragParking
                 if (PHus.PRutor[i].Ledig == true)
                 {
                     Console.WriteLine($"Parkeringsplats nr {i+1} är ledig");
+                    Console.WriteLine("Skriv in ditt regnr: ");
                     PHus.PRutor[i].Nummer = i + 1;
                     PHus.PRutor[i].Ledig = false;
-                    PHus.PRutor[i].Regnr = "QWE123";
+                    PHus.PRutor[i].Regnr = Console.ReadLine();
                     Console.WriteLine($"Parkering {PHus.PRutor[i].Nummer} är upptagen och innehåller {PHus.PRutor[i].Regnr}");
                     break;
                 }
                 i++;
             }
             i = 0;
-            while (i < antalRutor)
-            {
-                if (PHus.PRutor[i].Ledig == true)
-                {
-                    Console.WriteLine($"Parkeringsplats nr {i+1} är ledig");
-                    PHus.PRutor[i].Nummer = i + 1;
-                    PHus.PRutor[i].Ledig = false;
-                    PHus.PRutor[i].Regnr = "RTY123";
-                    Console.WriteLine($"Parkering {PHus.PRutor[i].Nummer} är upptagen och innehåller {PHus.PRutor[i].Regnr}");
-                    break;
-                }
-                i++;
-            }
-            Console.ReadKey();
+            //while (i < antalRutor)
+            //{
+            //    if (PHus.PRutor[i].Ledig == true)
+            //    {
+            //        Console.WriteLine($"Parkeringsplats nr {i+1} är ledig");
+            //        Console.WriteLine("Skriv in ditt regnr: ");
+            //        PHus.PRutor[i].Nummer = i + 1;
+            //        PHus.PRutor[i].Ledig = false;
+            //        PHus.PRutor[i].Regnr = Console.ReadLine();
+            //        Console.WriteLine($"Parkering {PHus.PRutor[i].Nummer} är upptagen och innehåller {PHus.PRutor[i].Regnr}");
+            //        break;
+            //    }
+            //    i++;
+            //}
             return i;
         }
     }
--- PragParking/Program.cs
+++ PragParking/Program.cs
@@ -12,57 +12,67 @@ namespace PragParking
             PRuta pRuta = new PRuta(); // Skapar P-Ruta
             PHus.skapaPHus();  // Skapar P-Hus
 
+            bool cont = true;
 
-
-
-            // Vi behöver ett fordon
-            Console.WriteLine("Välkommen till PragueParking.");
-            Console.WriteLine("Skriv in ditt regnr: ");
-            fordon.Regnr = Console.ReadLine();
-            Console.WriteLine($"Fordon: {fordon.Regnr}");
-
-
-            // Nu vill vi parkera fordonet
-            pRuta.Nummer = 1;
-            pRuta.Ledig = true;
-            Console.WriteLine($"Ruta {pRuta.Nummer} är ledig {pRuta.Ledig}");
-
-            if (pRuta.Ledig = true)
-                {
-                    pRuta.Regnr = fordon.Regnr;
-                    pRuta.Ledig = false;
-                }
-
-            Console.WriteLine($"Ruta {pRuta.Nummer} är ledig {pRuta.Ledig} där står {pRuta.Regnr}");
-            Console.WriteLine("Skriv in ditt regnr: ");
-            fordon.Regnr = Console.ReadLine();
-            Console.WriteLine($"Fordon: {fordon.Regnr}");
-            Console.WriteLine("Nu vill vi parkera fordonet");
-            pRuta.Nummer = 2;
-            pRuta.Ledig = true;
-            Console.WriteLine($"Ruta {pRuta.Nummer} är ledig {pRuta.Ledig}");
-            if (pRuta.Ledig = true)
+            while (cont)
             {
-                pRuta.Regnr = fordon.Regnr;
-                pRuta.Ledig = false;
+                Console.Clear();
+                Console.WriteLine("Välkommen till Prague Parking");
+                Console.WriteLine("Vad vill du göra? ");
+                Console.WriteLine("");
+                Console.WriteLine("1)  Parkera fordon");
+                Console.WriteLine("2)  Kolla efter lediga platser");
+                Console.WriteLine("3)  Exit");
+
+                string menu = Console.ReadLine();
+
+                switch (menu)
+                {
+                    case "1":
+                        int hittaPlats = PHus.hittaLedigPRuta();
+                        Console.WriteLine(hittaPlats);
+                        break;
+
+                    case "2":
+                        foreach (var Ruta in PHus.PRutor)
+                        {
+                            Console.WriteLine($"Ruta {Ruta.Nummer} är ledig {Ruta.Ledig}");
+                        }
+                        Console.ReadKey();
+                        break;
+
+                    case "3":
+                        cont = false; 
+                        break;
+
+                    default:
+                        Console.WriteLine("Ogiltigt val. Försök igen: ");
+                        Console.ReadKey();
+                        break;
+                }
             }
-            Console.WriteLine($"Ruta {pRuta.Nummer} är ledig {pRuta.Ledig} där står {pRuta.Regnr}");
+
 
             // Dags att skapa ett P hus
             // Kontrollerar att huset skapades
-            foreach (var Ruta in PHus.PRutor)
-            {
-                Console.WriteLine($"Ruta {Ruta.Nummer} är ledig {Ruta.Ledig}");
-            }
+            //foreach (var Ruta in PHus.PRutor)
+            //{
+            //    Console.WriteLine($"Ruta {Ruta.Nummer} är ledig {Ruta.Ledig}");
+            //}
 
-            // Dags att parkera ett fordon i P-Huset
+            //// Dags att parkera ett fordon i P-Huset
 
-            // 1. Hitta ledig plats
-            int hittaPlats = PHus.hittaLedigPRuta();
-            Console.WriteLine(hittaPlats);
-            // 2. Parkera fordonet
+            //// 1. Hitta ledig plats
+            //int hittaPlats = PHus.hittaLedigPRuta();
+            //Console.WriteLine(hittaPlats);
+            //// 2. Parkera fordonet
 
-            // Repetera fordonsparkering ett antal gånger
+            //// Repetera fordonsparkering ett antal gånger
+            //foreach (var Ruta in PHus.PRutor)
+            //{
+            //    Console.WriteLine($"Ruta {Ruta.Nummer} är ledig {Ruta.Ledig}");
+            //}
+            //Console.ReadKey();
 
         }
     }
